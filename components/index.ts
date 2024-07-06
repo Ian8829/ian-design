e0933ed5cff8624b2279d0ac2b5d9a191d1d639a
ian8829 <ghjkghj@naver.com>
2024-07-06 12:04:33 +0900

    add entry export

---
+++ components/index.ts
@@ -0,0 +1,31 @@
+import ISimpleCard from './card/ISimpleCard'
+import IImage from './image/IImage'
+import IInput from './input/IInput'
+import ITextArea from './textarea/ITextarea'
+import IMainContainer from './layout/IMainContainer'
+
+// timeline
+import ITimelineLinkedLine from './timeline/ITimelineLinkedLine'
+import ITimelineListItem from './timeline/ITimelineListItem'
+import ITimelineList from './timeline/ITimelineList'
+
+// form
+import IFormSubmitButton from './form/button/IFormSubmitButton'
+import IFormErrorMessage from './form/IFormErrorMessage'
+import IFormItemInput from './form/IFormItemInput'
+import IFormItemTextarea from './form/IFormItemTextarea'
+
+export {
+  ISimpleCard,
+  IImage,
+  IInput,
+  ITextArea,
+  IMainContainer,
+  ITimelineLinkedLine,
+  ITimelineListItem,
+  ITimelineList,
+  IFormSubmitButton,
+  IFormErrorMessage,
+  IFormItemInput,
+  IFormItemTextarea,
+}
